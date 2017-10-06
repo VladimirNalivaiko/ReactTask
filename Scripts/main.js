d055e2fde6202ae0d0f2484c74ca442440bdedfb
nalivaikovova <nalivaikovova@gmail.com>
2017-10-06 13:05:07 +0300

    validation added

--- Scripts/main.js
+++ Scripts/main.js
@@ -20,72 +20,64 @@ Rule.prototype.isRequired = function(){
     if(!this.isRequiredFlag){
         this.functionList.append(this.isRequired);
         this.isRequiredFlag = true;
+        return this;
     }
-    // if(this.right){
-    //     if(this.value){
-    //         this.right = true;
-    //     }else{
-    //         this.right = false;
-    //     }
-    // }
-    console.log("ISREQUIRED");
-    return this;
+    return this.value ? true : false;
 }
 
 Rule.prototype.min = function(value){
     if(!this.minFlag){
         this.functionList.append(this.min);
         this.minFlag = value;
+        return this;
     }
-    return this;
+    return this.value >= this.minFlag ? true : false;
 }
 
 Rule.prototype.max = function(value){
-    if(!this.minFlag){
-        this.functionList.append(this.min);
-        this.minFlag = value;
+    if(!this.maxFlag){
+        this.functionList.append(this.max);
+        this.maxFlag = value;
+        return this;
     }
-    this.max = value;
-    return this;
+    return this.value <= this.maxFlag ? true : false;
 }
 
 Rule.prototype.maxLength = function(value){
     if(!this.maxLengthFlag){
         this.functionList.append(this.maxLength);
         this.maxLengthFlag = value;
+        return this;
     }
-    this.maxLength = value;
-    return this;
+    return this.value.length <= this.maxLengthFlag ? true : false;
 }
 
 Rule.prototype.minLength = function(value){
     if(!this.minLengthFlag){
         this.functionList.append(this.minLength);
         this.minLengthFlag = value;
+        return this;
     }
-    // if(this.right){
-    //     console.log("minLength = " + this.value.length > value ? true : false);
-    //     this.right = this.value.length > value ? true : false;
-    // }
-    console.log("MINLENGTH");
-    return this;
+    return this.value.length >= this.minLengthFlag ? true : false;    
 }
 
-Rule.prototype.isInt = function(value){
+Rule.prototype.isInt = function(){
     if(!this.isIntFlag){
         this.functionList.append(this.isInt);
-        this.isIntFlag = value;
+        this.isIntFlag = true;
+        return this;
     }
-    return Number.isInteger(value);
+    return !isNaN(parseFloat(this.value)) && isFinite(this.value);
 }
 
-Rule.prototype.isEmail = function(email){
+Rule.prototype.isEmail = function(){
     if(!this.isEmailFlag){
         this.functionList.append(this.isEmail);
-        this.isEmailFlag = value;
+        this.isEmailFlag = true;
+        return this;
     }
     var emailReg = /^[a-z][a-zA-Z0-9_.]*(\.[a-zA-Z][a-zA-Z0-9_.]*)?@[a-z][a-zA-Z0-9]*\.[a-z]+(\.[a-z]+)?$/;
-    return emailReg.test(email);
+    return emailReg.test(this.value);
 }
 
 var Validator = function(){
@@ -94,19 +86,19 @@ var Validator = function(){
 
 Validator.validate = function(obj, rules){
     var properties = Object.getOwnPropertyNames(obj);
-    var propToRule = Object.getOwnPropertyNames(rules);//validationRules(name,age) 
-    console.log(obj);
-    console.log(rules);
+    var propToRule = Object.getOwnPropertyNames(rules);
     for(var i = 0; i < properties.length; i++){
-        console.log(obj[i]);
         rules[propToRule[i]].value = obj[properties[i]];
-        console.log(obj[properties[i]]);
     }
-    console.log(rules);
     for(var i = 0; i < properties.length; i++){
-        rules[i];
+        var rule = rules[properties[i]];
+        for(var j = 0; j < rule.functionList.length; j++){
+            if(!rule.functionList.at(j).data.call(rule)){
+                return false;
+            }
+        }
     }
-    console.log(rules);
+    return true;
 }
 
 var person1 = {
@@ -115,13 +107,27 @@ var person1 = {
 };
 var person2 = {
     name: "Igor",
-    age: 10
+    age: 7
 };
+var login = {
+    password: "123456",
+    email: "email@gmail.com"
+}
 
 var personValidationRules = { 
-    name: new Rule().isRequired().minLength(3).maxLength(10).isRequired(), 
-    age: new Rule().min(10) 
+    name: new Rule().isRequired().minLength(3).maxLength(10), 
+    age: new Rule().min(9) 
 };
 
-var validationPromise = Validator.validate(person1, personValidationRules);
+var loginValidationRules = { 
+    password: new Rule().isRequired().isInt(), 
+    email: new Rule().isRequired().minLength(6).isEmail()
+};
+
+var validationPromise1 = Validator.validate(person1, personValidationRules);
+var validationPromise2 = Validator.validate(person2, personValidationRules);
+var validationPromise3 = Validator.validate(login, loginValidationRules);
+console.log("person1 validate = " + validationPromise1);
+console.log("person2 validate = " + validationPromise2);
+console.log("login validate = " + validationPromise3);
 
