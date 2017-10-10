3ceef8e63f2c0fbe4e7b15cb822bce9cca2459b5
nalivaikovova <nalivaikovova@gmail.com>
2017-10-10 21:21:01 +0300

    Added code style fixes

--- Scripts/main.js
+++ Scripts/main.js
@@ -10,98 +10,100 @@ Rule.prototype.setValue = function (value) {
 }
 
 Rule.prototype.isRequired = function () {
-    this.functionArray.push(
-        (function (ruleValue) {
-            var ruleResult = {
-                error: "Value must be different from " + ruleValue,
-                value: ruleValue ? true : false
-            }
-            return ruleResult;
-        })
-    );
+    this.functionArray.push(function (ruleValue) {
+        var isValid = ruleValue ? true : false;
+        var message = "Value must be different from " + ruleValue;
+        var validationResult = {
+            errorMessage: isValid ? null : message,
+            isValid: isValid
+        }
+        return validationResult;
+    });
     return this;
 }
 
 Rule.prototype.min = function (value) {
     var parametr = value;
-    this.functionArray.push(
-        (function (ruleValue) {
-            var ruleResult = {
-                error: "Value must be greater than " + parametr,
-                value: ruleValue >= parametr ? true : false
-            }
-            return ruleResult;
-        })
-    );
+    this.functionArray.push(function (ruleValue, ruleName) {
+        var isValid = ruleValue >= parametr ? true : false;
+        var message = ruleName + '(' + ruleValue + ')' + " must be greater than " + parametr;
+        var validationResult = {
+            errorMessage: isValid ? null : message,
+            isValid: isValid
+        }
+        return validationResult;
+    });
     return this;
 }
 
 Rule.prototype.max = function (value) {
     var parametr = value;
-    this.functionArray.push(
-        (function (ruleValue) {
-            var ruleResult = {
-                error: "Value must be less than " + parametr,
-                value: ruleValue <= parametr ? true : false
-            }
-            return ruleResult;
-        })
-    );
+    this.functionArray.push(function (ruleValue, ruleName) {
+        var isValid = ruleValue <= parametr ? true : false;
+        var message = ruleName + '(' + ruleValue + ')' + " must be less than " + parametr;
+        var validationResult = {
+            errorMessage: isValid ? null : message,
+            isValid: isValid
+        }
+        return validationResult;
+    });
     return this;
 }
 
 Rule.prototype.maxLength = function (value) {
     var parametr = value;
-    this.functionArray.push(
-        (function (ruleValue) {
-            var ruleResult = {
-                error: "Length must be less than " + parametr,
-                value: ruleValue.length <= parametr ? true : false
-            }
-            return ruleResult;
-        })
-    );
+    this.functionArray.push(function (ruleValue, ruleName) {
+        var isValid = ruleValue.length <= parametr ? true : false;
+        var message = ruleName + "'s(" + ruleValue + ')' + " length" + '('
+            + ruleValue.length + ')' + " must be less than " + parametr;
+        var validationResult = {
+            errorMessage: isValid ? null : message,
+            isValid: isValid
+        }
+        return validationResult;
+    });
     return this;
 }
 
 Rule.prototype.minLength = function (value) {
     var parametr = value;
-    this.functionArray.push(
-        (function (ruleValue) {
-            var ruleResult = {
-                error: "Length must be greater than " + parametr,
-                value: ruleValue.length >= parametr ? true : false
-            }
-            return ruleResult;
-        })
-    );
+    this.functionArray.push(function (ruleValue, ruleName) {
+        var isValid = ruleValue.length >= parametr ? true : false;
+        var message = ruleName + "'s(" + ruleValue + ')' + " length" + '('
+            + ruleValue.length + ')' + " must be greater than " + parametr;
+        var validationResult = {
+            errorMessage: isValid ? null : message,
+            isValid: isValid
+        }
+        return validationResult;
+    });
     return this;
 }
 
 Rule.prototype.isInt = function () {
-    this.functionArray.push(
-        (function (ruleValue) {
-            var ruleResult = {
-                error: "It nust be a number",
-                value: !isNaN(parseFloat(ruleValue) && isFinite(ruleValue))
-            }
-            return ruleResult;
-        })
-    );
+    this.functionArray.push(function (ruleValue, ruleName) {
+        var isValid = !isNaN(parseFloat(ruleValue) && isFinite(ruleValue));
+        var message = ruleName + '(' + ruleValue + ')' + " must be a number";
+        var validationResult = {
+            errorMessage: isValid ? null : message,
+            isValid: isValid
+        }
+        return validationResult;
+    });
     return this;
 }
 
 Rule.prototype.isEmail = function () {
-    this.functionArray.push(
-        (function (ruleValue) {
-            var emailReg = /^[a-z][a-zA-Z0-9_.]*(\.[a-zA-Z][a-zA-Z0-9_.]*)?@[a-z][a-zA-Z0-9]*\.[a-z]+(\.[a-z]+)?$/;
-            var ruleResult = {
-                error: "It must be an email",
-                value: emailReg.test(ruleValue)
-            }
-            return ruleResult;
-        })
-    );
+    this.functionArray.push(function (ruleValue, ruleName) {
+        var emailReg = /^[a-z][a-zA-Z0-9_.]*(\.[a-zA-Z][a-zA-Z0-9_.]*)?@[a-z][a-zA-Z0-9]*\.[a-z]+(\.[a-z]+)?$/;
+        var isValid = emailReg.test(ruleValue);
+        var message = ruleName + '(' + ruleValue + ')' + " must be an email";
+        var validationResult = {
+            errorMessage: isValid ? null : message,
+            isValid: isValid
+        }
+        return validationResult;
+    });
     return this;
 }
 
@@ -112,23 +114,28 @@ var Validator = function () {
 Validator.validate = function (obj, rules) {
     var properties = Object.getOwnPropertyNames(obj);
     var propToRule = Object.getOwnPropertyNames(rules);
+    var errorsArray = [];
     for (var i = 0; i < properties.length; i++) {
         rules[propToRule[i]].value = obj[properties[i]];
     }
     for (var i = 0; i < properties.length; i++) {
         var rule = rules[properties[i]];
         for (var j = 0; j < rule.functionArray.length; j++) {
-            var result = rule.functionArray[j].call(rule, rules[propToRule[i]].value);
-            if (!result.value) {
-                return new Promise((resolve, reject) => {
-                    reject(result.error);
-                })
+            var result = rule.functionArray[j].call(rule, rules[propToRule[i]].value, propToRule[i]);
+            if (!result.isValid) {
+                errorsArray.push(result.errorMessage)
             }
         }
     }
-    return new Promise((resolve, reject) => {
-        resolve("Good")
-    })
+    if (errorsArray.length === 0) {
+        return new Promise((resolve, reject) => {
+            resolve("Good")
+        })
+    } else {
+        return new Promise((resolve, reject) => {
+            reject(errorsArray)
+        })
+    }
 }
 
 var person1 = {
@@ -141,17 +148,17 @@ var person2 = {
 };
 var login = {
     password: "123456",
-    email: "email@gmail.com"
+    email: "email1gmail.com"
 }
 
 var personValidationRules = {
-    name: new Rule().isRequired().minLength(3).maxLength(10),
+    name: new Rule().isRequired().minLength(4).maxLength(10),
     age: new Rule().min(12)
 };
 
 var loginValidationRules = {
     password: new Rule().isRequired().isInt(),
-    email: new Rule().isRequired().minLength(6).isEmail()
+    email: new Rule().isRequired().minLength(116).isEmail()
 };
 
 Promise.prototype.checkPromise = function () {
@@ -160,7 +167,7 @@ Promise.prototype.checkPromise = function () {
             console.log(result)
         },
         error => {
-            console.log(error)
+            error.map(item => { console.log(item) })
         }
     )
 }
